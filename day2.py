2ae42ec2056aa23003d98f343e51388505568675
Amaia Imaz Blanco <amaia.imazblanco1@nhs.net>
2023-12-04 10:00:16 +0000

    day 2 task 1

---
+++ day2.py
@@ -0,0 +1,46 @@
+import re
+
+TEST_INPUT = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+TEST_INPUT = TEST_INPUT.split("\n")
+
+with open("day2.txt") as f:
+    data = f.read().split("\n")
+
+max_color_values = {"red": 12, "green": 13, "blue": 14}
+
+
+def day2task1(data):
+    valid_games = []
+    for i, line in enumerate(data):
+        data[i] = line[line.find(":") + 2 :].split(";")
+
+    for i,game in enumerate(data):
+        valid_round = 0
+        for game_round in game:
+            
+            red = game_round.find('red')
+            blue = game_round.find('blue')
+            green = game_round.find('green')
+
+            if red!=-1:
+                if int(re.findall(r'(\d+)\s+red',game_round)[0])>12:
+                    valid_round+=1
+            if green!=-1:
+                if int(re.findall(r'(\d+)\s+green',game_round)[0])>13:
+                    valid_round+=1
+            if blue!=-1:
+                if int(re.findall(r'(\d+)\s+blue',game_round)[0])>14:
+                    valid_round+=1
+        if valid_round==0:
+            valid_games.append(i+1)
+        
+
+    return sum(valid_games)
+
+
+print(day2task1(data))
